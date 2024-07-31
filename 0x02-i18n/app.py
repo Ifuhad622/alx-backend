3779b4c296fafaeb303f149dd5baf66cbd0d3286
Ifuhad622 <ifuhad007@gmail>
2024-07-31 01:49:38 +0000

    0x02-i18n

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -8,19 +8,17 @@ from typing import Union, Dict
 from flask_babel import Babel, format_datetime
 from flask import Flask, render_template, request, g
 
-
 class Config:
-    """Represents a Flask Babel configuration.
-    """
+    """Represents a Flask Babel configuration."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -28,28 +26,22 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-
 def get_user() -> Union[Dict, None]:
-    """Retrieves a user based on a user id.
-    """
+    """Retrieves a user based on a user id."""
     login_id = request.args.get('login_as', '')
     if login_id:
         return users.get(int(login_id), None)
     return None
 
-
 @app.before_request
 def before_request() -> None:
-    """Performs some routines before each request's resolution.
-    """
+    """Performs some routines before each request's resolution."""
     user = get_user()
     g.user = user
 
-
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
+    """Retrieves the locale for a web page."""
     queries = request.query_string.decode('utf-8').split('&')
     query_table = dict(map(
         lambda x: (x if '=' in x else '{}='.format(x)).split('='),
@@ -66,11 +58,9 @@ def get_locale() -> str:
         return header_locale
     return app.config['BABEL_DEFAULT_LOCALE']
 
-
 @babel.timezoneselector
 def get_timezone() -> str:
-    """Retrieves the timezone for a web page.
-    """
+    """Retrieves the timezone for a web page."""
     timezone = request.args.get('timezone', '').strip()
     if not timezone and g.user:
         timezone = g.user['timezone']
@@ -79,15 +69,12 @@ def get_timezone() -> str:
     except pytz.exceptions.UnknownTimeZoneError:
         return app.config['BABEL_DEFAULT_TIMEZONE']
 
-
 @app.route('/')
 def get_index() -> str:
-    """API route: home/index page.
-    """
+    """API route: home/index page."""
     current_time = datetime.now(pytz.timezone(get_timezone()))
     formatted_time = format_datetime(current_time)
     return render_template('index.html', current_time=formatted_time)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
